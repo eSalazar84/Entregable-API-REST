ad3933f7ee0c48b349381ae6ab5b5e4c9d3a8798
Emiliano Salazar <salazaremiliano84@gmail.com>
2023-06-06 21:30:27 -0300

    implementando funcion updateOne()

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ Realizar el frontend de las operaciones CRUD que fueron provistas en clase
 (pueden usarlas, modificarlas o hacer otras distintas).
 */
 
-
 export class Users {
     constructor(name, email, phone) {
         this.name = name;
         this.email = email;
         this.phone = phone;
     }
+}
+
+//genero dinamicamente el contenido de la tabla
+function generateRows(item) {
+    const row = document.createElement("tr");
+    const userCell = document.createElement("td");
+    const emailCell = document.createElement("td");
+    const phoneCell = document.createElement("td");
+    const editCell = document.createElement("td");
+    const deleteCell = document.createElement("td");
+    const btnEdit = document.createElement("button");
+    btnEdit.onclick = updateOne(`${item.id}`);
+    btnEdit.textContent = "Editar";
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Eliminar"
+    userCell.textContent = item.name;
+    emailCell.textContent = item.email;
+    phoneCell.textContent = item.phone;
+    editCell.appendChild(btnEdit);
+    deleteCell.appendChild(btnDelete);
+    row.appendChild(userCell);
+    row.appendChild(emailCell);
+    row.appendChild(phoneCell);
+    row.appendChild(editCell);
+    row.appendChild(deleteCell);
+    tabla.appendChild(row);
+    document.body.appendChild(tabla);
 }
 
 const formAdd = document.getElementById("formAdd");
@@ -24,7 +50,7 @@ function handleSubmit() {
     const user = new Users(name.value, email.value, phone.value);
     addOne(user);
 }
-//Abre y cierra el modal para agregar un nuevo medidor
+//Abre y cierra el modal
 const openModal = document.getElementById("open-modal-add");
 const modal = document.getElementById("modal");
 const closeModal = document.getElementById("close-modal");
@@ -44,31 +70,8 @@ function getAll() {
         .then(data => {
             console.log(data);
             //Muestra el listado de todos los medidores
-            data.forEach(item => {                
-                const row = document.createElement("tr");
-                const userCell = document.createElement("td");
-                const emailCell = document.createElement("td");
-                const phoneCell = document.createElement("td");
-                const editCell = document.createElement("td");
-                const deleteCell = document.createElement("td");
-                const btnEdit = document.createElement("button");
-                btnEdit.type = "submit"
-                btnEdit.id = "open-modal-edit";
-                btnEdit.textContent = "Editar";
-                const btnDelete = document.createElement("button");
-                btnDelete.textContent = "Eliminar"
-                userCell.textContent = item.name;
-                emailCell.textContent = item.email;
-                phoneCell.textContent = item.phone;
-                editCell.appendChild(btnEdit);
-                deleteCell.appendChild(btnDelete);
-                row.appendChild(userCell);
-                row.appendChild(emailCell);
-                row.appendChild(phoneCell);
-                row.appendChild(editCell);
-                row.appendChild(deleteCell);
-                tabla.appendChild(row);
-                document.body.appendChild(tabla);
+            data.forEach(item => {
+                generateRows(item);
             });
         })
         .catch(err => console.error(err));
@@ -95,15 +98,11 @@ function deleteOne(user) {
         .then(res => res.json())
         .then(user => {
             console.log(user);
-
-
         })
         .catch(err => console.error(err));
 }
 
 function addOne(user) {
-
-    //Toma los datos cargados en el formaAdd 
     fetch(BASE_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -111,59 +110,12 @@ function addOne(user) {
     })
         .then(res => res.json())
         .then(user => {
-            console.log(user);
-            const tabla = document.getElementById("tabla");
-            const row = document.createElement("tr");
-            const userCell = document.createElement("td");
-            const emailCell = document.createElement("td");
-            const phoneCell = document.createElement("td");
-            const editCell = document.createElement("td");
-            const deleteCell = document.createElement("td");
-            const btnEdit = document.createElement("button");
-            btnEdit.textContent = "Editar";
-            const btnDelete = document.createElement("button");
-            btnDelete.textContent = "Eliminar"
-            userCell.textContent = user.name;
-            emailCell.textContent = user.email;
-            phoneCell.textContent = user.phone;
-            editCell.appendChild(btnEdit);
-            deleteCell.appendChild(btnDelete);
-            row.appendChild(userCell);
-            row.appendChild(emailCell);
-            row.appendChild(phoneCell);
-            row.appendChild(editCell);
-            row.appendChild(deleteCell);
-            tabla.appendChild(row);
-            document.body.appendChild(tabla);
+            generateRows(user);
         })
         .catch(err => console.error(err));
 }
-//edit a new resource
-const updatedUser = {
-    name: "Jorge 'el profe' Sardón",
-    email: "giorgioDJ@dero.com",
-    phone: "(2314) 232323223",
-};
-
-
-const formEdit = document.getElementById("open-modal-edit");
-const openModalEdit = document.getElementById("open-modal-edit");
-const modalEdit = document.getElementById("modal-edit");
-const closeModalEdit = document.getElementById("close-modal-edit");
-openModalEdit.addEventListener("click", () => {
-    modalEdit.showModal();
-});
-closeModalEdit.addEventListener("click", () => {
-    modalEdit.close();
-});
-
-formEdit.addEventListener("click", ()=>{
-    updateOne();
-})
-
 
 function updateOne(id, user) {
-    BASE_URL.find(id);
     fetch(BASE_URL + `/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -172,11 +124,6 @@ function updateOne(id, user) {
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            const user = data.find(id)
-            console.log(user);
-
-
-
 
 
         })
@@ -184,3 +131,4 @@ function updateOne(id, user) {
 }
 
 getAll();
+
